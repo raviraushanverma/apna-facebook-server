f3948b8abb3687913698613920ace2d6c5c8a402
kamlesh verma <kamleshverma@kamleshs-MacBook-Pro.local>
2024-02-08 13:11:19 +0530

    donee

--- websocket.js
+++ websocket.js
@@ -4,13 +4,16 @@ export const webSocketCallBack = (socket) => {
   socket.emit("all-connected-users", [...socketToUserIdMapping.values()]);
 
   socket.on("user-connected", (userId) => {
-    console.log("user connected with ID ", userId);
+    console.log("user connected with ID ==> ", userId);
     socketToUserIdMapping.set(`${socket.id}`, userId);
     socket.broadcast.emit("other-user-connected", userId);
   });
 
   socket.on("disconnect", function () {
-    console.log("user disconnected with ID ", `${socket.id}`);
+    console.log(
+      "user disconnected with ID ==> ",
+      socketToUserIdMapping.get(`${socket.id}`)
+    );
     socket.broadcast.emit(
       "other-user-disconnected",
       socketToUserIdMapping.get(`${socket.id}`)
